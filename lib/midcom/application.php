7ccfb5a82d8e0a5b8d32b55c5bee0f322bea2960
flack <flack@contentcontrol-berlin.de>
2021-03-29 11:29:47 +0200

    import

--- lib/midcom/application.php
+++ lib/midcom/application.php
@@ -82,7 +82,6 @@ class midcom_application extends Kernel
 
     public function __construct(string $environment, bool $debug)
     {
-        midcom_compat_environment::initialize();
         $this->request = Request::createFromGlobals();
         $this->cfg = new midcom_config;
         parent::__construct($environment, $debug);
@@ -329,10 +328,10 @@ class midcom_application extends Kernel
      * Header you sent must go through this function or it might be lost later on;
      * this is especially important with caching.
      */
-    public function header(string $header, int $response_code = null)
+    public function header(string $header, int $response_code = 0)
     {
         $this->cache->content->register_sent_header($header);
-        midcom_compat_environment::get()->header($header, true, $response_code);
+        midcom_compat_environment::header($header, true, $response_code);
     }
 
     /**
